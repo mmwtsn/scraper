28521e4c7e4f19fea57493eee50bdb4ffb24800f
M. Maxwell Watson <hi@mmwtsn.com>
2014-07-14 23:18:06 -0400

    passing .search_for spec

--- scraper.rb
+++ scraper.rb
@@ -3,4 +3,22 @@ require 'bundler'
 Bundler.require
 
 module Scraper
+  require 'open-uri'
+
+  def self.search_for(query, location)
+    result = false
+
+    open(location) do |f|
+      f.each do |line|
+        if /#{query}/ =~ line
+          result = true
+          break
+        end
+      end
+
+      return result
+    end
+
+  end
+
 end
