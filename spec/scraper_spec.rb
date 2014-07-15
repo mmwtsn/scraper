5c8818c5d933480b530c118b0083650ed1357412
M. Maxwell Watson <hi@mmwtsn.com>
2014-07-14 22:54:15 -0400

    failing .search_for spec

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,4 +1,21 @@
 require 'spec_helper'
 
 describe Scraper do
+  describe '.search_for(query, location)' do
+
+    # Issa's snail haiku contains 'snail'
+    it 'returns true when found' do
+      response = Scraper.search_for('snail', 'http://issa-haiku.org/snail.html')
+
+      expect(response).to be_true
+    end
+
+    # Issa's snail haiku does not contain 'snake'
+    it 'returns false when not found' do
+      response = Scraper.search_for('snake', 'http://issa-haiku.org/snail.html')
+
+      expect(response).to be_false
+      expect(response).to_not be_nil # Ensure RSpec does not report a false positive
+    end
+  end
 end
