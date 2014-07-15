5f27b80a9b1e071db21c475cdaaef8dc48d08698
M. Maxwell Watson <hi@mmwtsn.com>
2014-07-14 22:52:35 -0400

    adds and configures webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,19 @@
-require 'webmock'
+require 'webmock/rspec'
 require_relative '../scraper'
 
-# Ensure the expect syntax is used
+body = IO.read('./spec/support/issa-haiku.html')
+
+# Configure RSpec
 RSpec.configure do |config|
+  # Ensure the expect syntax is used
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  # Stub external HTTP requests with WebMock
+  config.before(:each) do
+    stub_request(:get, /issa-haiku.org/).
+      with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+      to_return(status: 200, body: body, headers: {})
   end
 end
