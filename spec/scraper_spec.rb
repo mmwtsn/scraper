a2094849a481cc7ebd3f6a0cbbda568111e5a029
M. Maxwell Watson <hi@mmwtsn.com>
2014-07-14 23:37:21 -0400

    adds spec for return length

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -4,17 +4,17 @@ describe Scraper do
   describe '.search_for(query, location)' do
 
     # Issa's snail haiku contains 'snail'
-    it 'returns an array of line numbers' do
+    it 'returns an array' do
       result = Scraper.search_for('snail', 'http://issa-haiku.org/snail.html')
 
       expect(result).to be_an_instance_of(Array)
     end
 
-    # Issa's snail haiku contains 'snlowly' twice
-    it 'returns correct number of matches' do
-      result = Scraper.search_for('slowly', 'http://issa-haiku.org/snail.html')
+    # Issa's mock haiku web page contains 'haiku' three times, two of which are lowercase
+    it 'ignores case of query' do
+      result = Scraper.search_for('haiku', 'http://issa-haiku.org/snail.html')
 
-      expect(result.length).to eq(2)
+      expect(result.length).to eq(4)
     end
 
     # Issa's snail haiku does not contain 'snake'
