1a7cd71c004fe6eb9897c05e7e4334aad9652528
M. Maxwell Watson <hi@mmwtsn.com>
2014-07-17 10:19:44 -0400

    sketches out the scraper class

--- scraper.rb
+++ scraper.rb
@@ -2,66 +2,16 @@
 require 'bundler'
 Bundler.require
 
-module Scraper
-  require 'open-uri'
+# Load Ruby Libraries
+require 'open-uri'
 
-  def self.search_for(query, location, options = {})
-    result = {}
-    result[location] = []
+class Scraper
+  attr_reader   :found, :results
+  attr_accessor :query
 
-    check(query, location, result, options)
-  end
-
-  private
-
-  def self.build_regexp(query, options = {})
-    if options[:ignore_case]
-      regexp = /#{query}/i
-    else
-      regexp = /#{query}/
-    end
-
-    regexp
-  end
-
-  def self.build_location(location)
-    if location.instance_of?(String)
-      location = [location]
-    end
-
-    location
-  end
-
-  def self.open_location(location, regexp, result)
-    location.each do |loc|
-
-      open(loc) do |f|
-        f.each do |line|
-          if regexp =~ line
-            result[loc] << f.lineno
-          end
-        end
-      end
-
-    end
-  end
-
-  def self.build_result(result, location)
-    location.each do |loc|
-      if result[loc].empty?
-        result = false
-      end
-    end
-
-    result
-  end
-
-  def self.check(query, location, result, options = {})
-    regexp = build_regexp(query, options)
-
-    location = build_location(location)
-    open_location(location, regexp, result)
-
-    result = build_result(result, location)
+  def initialize(query)
+    @query   = query
+    @found   = false
+    @results = []
   end
 end
